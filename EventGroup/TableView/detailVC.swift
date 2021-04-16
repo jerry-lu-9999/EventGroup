24056bf512ee343de7a01c28c2bb66221d0d15fd
jerry-lu-9999 <ljh274163788@gmail.com>
2021-04-16 09:44:17 -0400

    Created README.md, ready for Core Data

--- EventGroup/TableView/detailVC.swift
+++ EventGroup/TableView/detailVC.swift
@@ -12,24 +12,19 @@ class detailVC: UIViewController {
     @IBOutlet weak var eventImage: UIImageView!
     @IBOutlet weak var timeStamp: UILabel!
     @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var likeButton: UIBarButtonItem!
     
     var details : Event!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.title = details.title
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onTapLike(_ sender: Any) {
+        
     }
-    */
-
+    
 }
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # EventGroup
+
+EventGroup is an amazing app that can get you the latest events happening in the states!
+
+## API usage
+
+https://platform.seatgeek.com/#events
+
+## Description
+
+This app is written in Swift (UIKit)
+
+There are three main controllers.
+1. NavigationController (MainVC). It is mostly empty
+2. TableViewController (TVC). 
+3. ViewController (detailVC). A detail view for each table cell
+
+We also have a UISearchController in code embedded in TableViewController (TVC), it gives us the searchBar on the top of the tableView cells. 
+
+The app also uses Core Data to persist between app launches. 
+
+## Build and Deployment target
+
+This project is built in Xcode 12.4
+Deployment target : iOS 12.1
+
+
+
